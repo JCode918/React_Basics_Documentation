0a2c58695453503eca64f69103173c7481d7d8bd
JCode918 <Willard.Joseph26@gmail.com>
2020-03-28 16:41:53 -0400

    Chapter 10 - Lifting The State Up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import './App.css';
-import NameForm from './components_chapter9/NameForm'
-import EssayForm from './components_chapter9/EssayForm'
-import SelectForm from './components_chapter9/SelectForm'
-import Reservation from './components_chapter9/Reservation'
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       {/*  <NameForm />
-        <EssayForm />
-        <SelectForm /> */}
-        <Reservation/>
+      
       </header>
     </div>
   );
